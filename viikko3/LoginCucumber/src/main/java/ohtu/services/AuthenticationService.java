ddc222dc8af4c74fe536693f20d7c1e426172ba7
Matias Himanen <matias.himanen@helsinki.fi>
2020-11-11 15:58:55 +0200

    .

---
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,74 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        // validity check of username and password
+
+
+
+        if (username.length() < 3 || password.length() < 8){
+            System.out.println("liian lyhyt");
+            return true;
+        }
+
+
+        char[] un = username.toCharArray();
+
+    for (char c : un) {
+        if(!Character.isLetter(c)) {
+            System.err.println("nimessä numeroita");
+            return true;
+        }
+    }
+
+               char[] pw = password.toCharArray();
+
+    for (char c : pw) {
+        if(!Character.isLetter(c)) {
+
+            return false;
+        }
+        
+    }
+
+
+        return true;
+    }
+}
